7a4b02eb174d4279e18462355af9bdc2f5a0076e
Parish-Gawari <parishk1024@gmail.com>
2025-10-04 12:19:08 +0530

    streaming done

---
+++ src/app/product-reviews/page.tsx
@@ -0,0 +1,17 @@
+import { Product } from "@/components/product";
+import { Reviews } from "@/components/reviews";
+import { Suspense } from "react";
+
+export default function ProductReviews() {
+  return (
+    <div>
+      <h1>Product Reviews</h1>
+      <Suspense fallback={<p>Loading Product details........ </p>}>
+        <Product />
+      </Suspense>
+      <Suspense fallback={<p>Loading Product details........</p>}>
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
