b14651d0f9e55b38a08ac2fe9e54c7bcd112e2da
Parish-Gawari <parishk1024@gmail.com>
2025-10-04 13:17:57 +0530

    third packages done

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,9 +1,11 @@
+import ServerRoutePage from '@/components/ImageSlider';
 import { serverSideFunction } from '../../utils/server-utils';
 export default function serverSide() {
    const res = serverSideFunction();
   return (
     <>
       <h1>This is Server Side Component {res}</h1>
+      <ServerRoutePage/>
     </>
   );
 }
