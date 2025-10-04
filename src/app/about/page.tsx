35c950474d93c09e2e16b403e8085cf681d3f651
Parish-Gawari <parishk1024@gmail.com>
2025-10-04 11:47:52 +0530

    dynamic rendering done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function AboutPage(){
+import { cookies } from "next/headers"
+
+export default async function AboutPage(){
+    const cookieStore = await cookies()
+    const theme = cookieStore.get("theme") || 'dark'
+    console.log(theme)
     console.log('Hello from Server Component')
     return <h1>Hello There {new Date().toLocaleTimeString()}</h1>
 }
